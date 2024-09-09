ddfb8ed86dce0804365a1af1dd9496d015abbca0
windi al azmi <windi.azmy@gmail.com>
2024-09-09 19:35:29 +0700

    15 menit

--- index.js
+++ index.js
@@ -2,33 +2,15 @@ const getLengthLetter = (data) => {
   
    let getData = data.split("");
    let newArray = {};
-
-
-       
      
    getData.forEach(element => {
         if(newArray[element]) {
-            console.log(getData[element])
+            newArray[element]++
         }else{
-            console.log('bukan')
-            n
+            newArray[element]  = 1
         }
     });
-   console.log(newArray, 'ini new array');
-    // let newData = [];
-    // let getData =  data.split("");
-    // console.log(getData, 'ini get data');
-    // let prevData = data[0];
-    // console.log(prevData, 'ini prev data')
-    // for(let i=0; i < data.length; i++){
-    //     let dataBaru = `${data[i]} : ${1}`
-    //     let dataDuplicate = `${data[i]} : ${1}`
-
-    //     if(prevData !== data[i]) newData.push(dataBaru)
-    //     // else newData.push(dataDuplicate)
-    // }
-
-    // console.log(newData, 'ini new data');
+   return newArray;
 }
 
 
