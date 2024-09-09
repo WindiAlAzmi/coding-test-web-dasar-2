6293346383ffd5f23e0e2225e85a9fdb0ecee6e6
windi al azmi <windi.azmy@gmail.com>
2024-09-09 19:26:14 +0700

    10 menit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const getLengthLetter = (data) => {
   
-    let newData = [];
-    let getData =  data.split("");
-    console.log(getData, 'ini get data');
-    let prevData = data[0];
-    console.log(prevData, 'ini prev data')
-    for(let i=0; i < data.length; i++){
-        let dataBaru = `${data[i]} : ${i}`
-        if(prevData !== data[i]) newData.push(dataBaru)
+  let newDataArray = data.reduce((prev, new) =>  {
+    if(prev === new) {
+       return {new : 1}
+    }else {
+        return
     }
+  })
+    // let newData = [];
+    // let getData =  data.split("");
+    // console.log(getData, 'ini get data');
+    // let prevData = data[0];
+    // console.log(prevData, 'ini prev data')
+    // for(let i=0; i < data.length; i++){
+    //     let dataBaru = `${data[i]} : ${1}`
+    //     let dataDuplicate = `${data[i]} : ${1}`
 
-    console.log()
+    //     if(prevData !== data[i]) newData.push(dataBaru)
+    //     // else newData.push(dataDuplicate)
+    // }
+
+    // console.log(newData, 'ini new data');
 }
 
 
